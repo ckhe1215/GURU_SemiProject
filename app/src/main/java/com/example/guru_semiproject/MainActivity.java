ad127cfcd1fcbcb421ddb32f9ccd417bff13492c
ckhe1215 <ckhe1215@gmail.com>
2019-07-03 14:55:14 +0900

    first commit

--- app/src/main/java/com/example/guru_semiproject/MainActivity.java
+++ app/src/main/java/com/example/guru_semiproject/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 테스트코드 1
+        // Commit & Push 테스트
     }
 }
